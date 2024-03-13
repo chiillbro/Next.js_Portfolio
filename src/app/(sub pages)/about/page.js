a09c1260d46dc0d2c2d5d72fb6653b0a819e83d2
Siva Sankar Reddy <aasamsivasankarreddynani143@gmail.com>
2024-03-13 14:17:05 +0530

    made some changes to responsive design of about details

--- src/app/(sub pages)/about/page.js
+++ src/app/(sub pages)/about/page.js
@@ -25,7 +25,7 @@ export default function About() {
         </RenderModel>
       </div>
       <div className="relative w-full h-screen flex flex-col items-center justify-center">
-        <div className="absolute text-center top-1/2 sm:top-[60%] left-1/2 -translate-y-1/2 -translate-x-1/2">
+        <div className="absolute text-center top-[60%] xs:top-1/2 sm:top-[60%] left-1/2 -translate-y-1/2 -translate-x-1/2">
           <h1 className="font-bold text-5xl xs:text-6xl sm:text-7xl  text-accent">
             Hey there! 👋
           </h1>
